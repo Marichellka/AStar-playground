cc40fd39be30564704956db0e347b2eb0a8d29c4
Marichka <marichka.v.richtsi@gmail.com>
2021-03-06 17:11:07 +0200

    Create class Node

---
+++ Project/Project/Node.cs
@@ -0,0 +1,21 @@
+﻿namespace Project
+{
+    public class Node
+    {
+        private bool Barrier { get; } 
+        private int Cost { get; set; }
+        private int X = 0;
+        private int Y = 0;
+        private bool Marked { get; set; }
+        private Node Parent { get; set; }
+
+        public Node(int column, int row, int value, char elem)
+        {
+            X = column;
+            Y = row;
+            Cost = value;
+            if (elem == 'X') Barrier = true;
+            else Barrier = false;
+        } 
+    }
+}
