9346c8654bfac80e551e7b94f91c06d4da27786f
Marichka <marichka.v.richtsi@gmail.com>
2021-03-06 18:41:51 +0200

    Create class Grid
    Add methods GetGrid and ConvertToNodesObj

---
+++ Project/Project/Grid.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Project
+{
+    public class Grid
+    {
+        public List<Node[]> GetGrid(string path)
+        {
+            List<Node[]> grid = new List<Node[]>();
+            using (var sr= new StreamReader(path))
+            {
+                int numberOfRow = 0;
+                while (!sr.EndOfStream)
+                {
+                    string row = sr.ReadLine();
+                    grid.Add(ConvertToNodesObj(row, numberOfRow));
+                    numberOfRow++;
+                }
+            }
+
+            return grid;
+        }
+
+        private Node[] ConvertToNodesObj(string str, int rowNumber)
+        {
+            Node[] currentRow = new Node[str.Length/2];
+            for (int i = 0; i < str.Length; i ++)
+            {
+                currentRow[i/2] = new Node(i / 2, rowNumber, str[i]);
+                i++;
+            }
+            return currentRow;
+        }
+    }
+}
