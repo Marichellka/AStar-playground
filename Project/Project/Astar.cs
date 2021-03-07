a227856dc9808daf8b209e3814a1acc26c60de79
Marichka <marichka.v.richtsi@gmail.com>
2021-03-07 11:17:52 +0200

    Create Astar.cs

---
+++ Project/Project/Astar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Project
+{
+    public class Astar
+    {
+        public void AlgorithmAstar(Node[][] grid, Node startNode, Node endNode)
+        {
+            List<Node> openNodes = new List<Node>();
+            openNodes.Add(startNode);
+            startNode.Cost = 0;
+            startNode.EstimatedCost = startNode.Cost + HeuristicFunction(startNode, endNode);
+            while (openNodes.Count > 0)
+            {
+                Node currentNode = MinNodeByEstimatedCost(openNodes);
+                if (currentNode == endNode)
+                {
+                    return;
+                }
+
+                openNodes.Remove(currentNode);
+                currentNode.Marked = true;
+            }
+        }
+        
+        public Node MinNodeByEstimatedCost(List<Node> openNodes)
+        {
+            return null;
+        }
+        public int HeuristicFunction(Node current, Node end)
+        {
+            return 0;
+        }
+    }
+}
